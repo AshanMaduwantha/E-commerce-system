07dcff44f9ea42b7cbfbc54110567e20e3b2cb2d
Oshadhie Indudunu <153111583+Oshadhie@users.noreply.github.com>
2026-03-27 15:00:25 +0530

    create product services

---
+++ product-service/swagger.js
@@ -0,0 +1,16 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Product Service API",
+      version: "1.0.0",
+    },
+  },
+  apis: [__dirname + "/routes/productRoutes.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
